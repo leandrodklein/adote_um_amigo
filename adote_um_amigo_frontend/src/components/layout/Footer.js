1248a439e8706667bc865e08d3a60e071684e1a7
leandrodklein <leklein@gmail.com>
2022-05-04 20:23:38 -0300

    project backend and frontend

---
+++ adote_um_amigo_frontend/src/components/layout/Footer.js
@@ -0,0 +1,9 @@
+import styles from "./Footer.module.css";
+
+function Footer() {
+  return <footer className={styles.footer}>
+    <p><b>Adote Um Amigo</b> &copy; 2022</p>
+  </footer>
+}
+
+export default Footer
